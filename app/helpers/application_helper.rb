431644fbdd82e095a17e2829b33dc42ac2c3a6e5
EC2 Default User <drew.s.gordon@gmail.com>
2013-12-04 03:25:20 +0000

    format facet price as currrency

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,8 +4,8 @@ module ApplicationHelper
     number_to_currency(args[:document][args[:field]], :unit => "$")
   end
   
-  def price_to_currency_facet args
-    number_to_currency(args[:document][args[:solr_field]], :unit => "$")
+  def price_to_currency_facet value
+    number_to_currency(value, :unit => "$")
   end
   
   
